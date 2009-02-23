6ae9661202d875c7ec6642ec01310fef4f1437c9
ray <ray@19b8ce72-01ed-11de-9dc8-6539b95b8247>
2009-02-23 21:06:50 +0000

    Initial commit.
    git-svn-id: https://svn.artran.webfactional.com/mingus/trunk@1 19b8ce72-01ed-11de-9dc8-6539b95b8247

---
+++ templatetags/cms_tags.py
@@ -0,0 +1,46 @@
+from django import template
+
+import re
+
+from cms.models import *
+
+image_match_regex = re.compile(r'{{\s*IMAGE\[[\w-]*\]\s*}}')
+slug_match_regex = re.compile(r'\[([\w-]*)\]')
+
+register = template.Library()
+
+@register.filter
+def add_images(article, lang):
+    '''
+    Gets the translated body text and goes through it resolving {{ IMAGE[<SLUG>] }} into the correct url for the image
+    with slug=<SLUG>
+    '''
+    body = article.get_i18n_body(lang)
+    matches = image_match_regex.finditer(body)
+    if matches:
+        images = article.images.all()
+        
+        # To manipulate the body text safely we need to reverse the iterator
+        for match in reversed(list(matches)):
+            slug = slug_match_regex.search(match.group()).group(1)
+            try:
+                image = images.get(slug=slug)
+                image_url = image.get_absolute_url()
+            except AssertionError:
+                print 'Multiple images found with the slug %s' % slug
+                image_url = 'MultipleImagesExist'
+            except Image.DoesNotExist:
+                print 'No images with the slug "%s" related to article with slug "%s"' % (slug, article.slug)
+                image_url = 'NoImageFound'
+            
+            body = body[:match.start()] + image_url + body[match.end():]
+    return body
+add_images.is_safe = True
+
+@register.filter
+def i18n_section_name(section, lang):
+    return section.get_i18n_name(lang)
+
+@register.filter
+def i18n_article_title(article, lang):
+    return article.get_i18n_title(lang)
---
+++ views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import Http404
+from django.template import RequestContext
+
+from cms.models import *
+
+def index(request):
+    first_section = Section.live_objects.all()[0]
+    return section(request, first_section.slug)
+
+def section(request, slug):
+    live_articles = Article.live_objects.filter(section__slug=slug)
+    
+    # Try to get a "home_page" article, if there are none use any article
+    articles = live_articles.filter(home_page=True).order_by('?')
+    if articles.count() > 0:
+        the_article = articles[0]
+    else:
+        the_article = live_articles.order_by('?')[0]
+    return article(request, the_article.slug)
+
+def article(request, slug):
+    article = get_object_or_404(Article, slug=slug)
+    if not article.is_live():
+        raise Http404
+    
+    sections = Section.live_objects.filter(parent__isnull=True)
+    live_articles = Article.live_objects.all()
+    
+    related = article.get_live_related()
+    
+    active = article.section
+    while active.parent:
+        active = active.parent
+    return render_to_response('cms/article.html', {'sections': sections, 'article': article, 'active_section': active,
+                        'related': related, 'in_this_section': live_articles, 'session': request.session, 'lang': request.LANGUAGE_CODE})
