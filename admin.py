931a4e804a6fbe385e79d028a3af56747defae2a
ray <ray@19b8ce72-01ed-11de-9dc8-6539b95b8247>
2009-02-26 23:09:13 +0000

    Switch to Django 1.0
    git-svn-id: https://svn.artran.webfactional.com/mingus/trunk@4 19b8ce72-01ed-11de-9dc8-6539b95b8247

---
+++ admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+from mingus.models import *
+
+class ArticleImageInline(admin.StackedInline):
+    model = ArticleImage
+
+class SectionImageInline(admin.StackedInline):
+    model = SectionImage
+
+class SectionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = (SectionImageInline,)
+
+class TransSectionAdmin(admin.ModelAdmin):
+    list_display = ('trans_name', 'section', 'lang')
+    list_filter = ['lang']
+
+class ArticleAdmin(admin.ModelAdmin):
+    save_on_top = True
+    list_filter = ('section', 'created_by')
+    search_fields = ('title',)
+    list_display = ('title', 'live_from', 'live_to', 'is_live')
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = (ArticleImageInline,)
+    filter_horizontal = ('related',)
+
+class TransArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'article', 'lang')
+    list_filter = ['lang']
+
+class ArticleImageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+class SectionImageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Language)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(TransSection, TransSectionAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(TransArticle, TransArticleAdmin)
+admin.site.register(ArticleImage, ArticleImageAdmin)
+admin.site.register(SectionImage, SectionImageAdmin)
--- models.py
+++ models.py
@@ -22,15 +22,14 @@ class LiveArticleManager(models.Manager):
 class Language(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=2, help_text='Use the ISO 639-1 2-letter language code (see wikipedia)')
-    class Admin:
-        pass
+
     def __unicode__(self):
         return u'%s (%s)' % (self.name, self.code)
 
 class Section(models.Model):
     name = models.CharField(max_length=50, unique=True)
     live = models.BooleanField(default=False)
-    slug = models.SlugField(prepopulate_from=('name',), help_text='Auto generated')
+    slug = models.SlugField(help_text='Auto generated')
     block_img = models.ImageField(upload_to='block-images', blank=True)
     thumbnail_img = models.ImageField(upload_to='icons', blank=True)
     sort = models.SmallIntegerField(help_text='Lower numbers sort earlier.')
@@ -64,17 +63,12 @@ class Section(models.Model):
             return u'%s (not live)' % self.name
     class Meta:
         ordering = ['sort']
-    class Admin:
-        pass
 
 class TransSection(models.Model):
     """The translation of a section's name"""
     lang = models.ForeignKey(Language)
     section = models.ForeignKey(Section)
     trans_name = models.CharField(max_length=50, unique=True)
-    class Admin:
-        list_display = ('trans_name', 'section', 'lang')
-        list_filter = ['lang']
     class Meta:
         unique_together = ('lang', 'section')
     def __unicode__(self):
@@ -94,8 +88,8 @@ class Article(models.Model):
     created_by = models.ForeignKey(User, editable=False)
     last_edited_at = models.DateTimeField(blank=True, editable=False)
     last_edited_by = models.ForeignKey(User, related_name='edited_articles', editable=False)
-    related = models.ManyToManyField('self', filter_interface=models.HORIZONTAL, blank=True)
-    slug = models.SlugField(prepopulate_from=('title',), unique=True, help_text='Auto generated')
+    related = models.ManyToManyField('self', blank=True)
+    slug = models.SlugField(unique=True, help_text='Auto generated')
     section = models.ForeignKey(Section, related_name='articles')
     
     # Managers
@@ -146,11 +140,6 @@ class Article(models.Model):
             return self.title
         else:
             return u'%s (not live)' % self.title
-    class Admin:
-        save_on_top = True
-        list_filter = ('section', 'created_by')
-        search_fields = ('title',)
-        list_display = ('title', 'live_from', 'live_to', 'is_live')
 
 class TransArticle(models.Model):
     """The translation of an article's title and body text"""
@@ -158,21 +147,18 @@ class TransArticle(models.Model):
     article = models.ForeignKey(Article)
     title = models.CharField(max_length=100)
     body = models.TextField(help_text='For local images use {{ IMAGE[&lt;SLUG&gt;] }} or /media/cms_images/&lt;IMG-FILE&gt; for the url.')
-    class Admin:
-        list_display = ('title', 'article', 'lang')
-        list_filter = ['lang']
     class Meta:
         unique_together = ('lang', 'article')
     def __unicode__(self):
         return u'%s (%s, %s)' % (self.title, self.article.title, self.lang.name)
 
 class Image(models.Model):
-    name = models.CharField(max_length=30, core=True)
-    slug = models.SlugField(prepopulate_from=('name',), blank=True, help_text='Auto generated but can be overridden')
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(blank=True, help_text='Auto generated but can be overridden')
     caption = models.CharField(max_length=50, blank=True)
     height = models.IntegerField(null=True, blank=True)
     width = models.IntegerField(null=True, blank=True)
-    image = models.ImageField(upload_to='cms_images', width_field='width', height_field='height', core=True)
+    image = models.ImageField(upload_to='cms_images', width_field='width', height_field='height')
     created_at = models.DateTimeField(blank=True, editable=False)
     created_by = models.ForeignKey(User, editable=False)
     
@@ -198,19 +184,17 @@ class Image(models.Model):
         ordering = ['id']
 
 class ArticleImage(Image):
-    article = models.ForeignKey(Article, edit_inline=models.STACKED, related_name='images')
-    class Admin:
-        pass
+    article = models.ForeignKey(Article, related_name='images')
+
     # I've comented this out after creation of the tables because it breaks the inline-editing in Articles.
     # As the uniqueness test is done in the db this shouldn't be an issue.
     #class Meta:
     #    unique_together = (('slug', 'article'),)
 
 class SectionImage(Image):
-    section = models.ForeignKey(Section, edit_inline=models.STACKED, related_name='images')
+    section = models.ForeignKey(Section, related_name='images')
     thumbnail_img = models.ImageField(upload_to='icons', help_text='85 x 85 circular images')
-    class Admin:
-        pass
+
     # I've comented this out after creation of the tables because it breaks the inline-editing in Sections.
     # As the uniqueness test is done in the db this shouldn't be an issue.
     #class Meta:
