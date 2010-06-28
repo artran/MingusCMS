5d3195712fd389fa8c7f762e1c3281768e58daf3
Ray Tran <ray@artran.co.uk>
2010-06-28 22:02:14 +0100

    Fixed killserver script so that it kills child processes.

--- bin/killserver.sh
+++ bin/killserver.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 PID_DIR=/tmp
-RUNSERVER_PID=$PID_DIR/runserver.pid
-CMSSERVER_PID=$PID_DIR/cmsserver.pid
+RUNSERVER_PID_FILE=$PID_DIR/runserver.pid
+CMSSERVER_PID_FILE=$PID_DIR/cmsserver.pid
 
-kill -TERM `cat $RUNSERVER_PID`
-kill -TERM `cat $CMSSERVER_PID`
+RUNSERVER_PID=`cat $RUNSERVER_PID_FILE`
+CMSSERVER_PID=`cat $CMSSERVER_PID_FILE`
 
-rm $RUNSERVER_PID
-rm $CMSSERVER_PID
+for child in $(ps -o pid,ppid -ax | \
+   awk "{ if ( \$2 == $RUNSERVER_PID || \$2 == $CMSSERVER_PID) { print \$1 }}")
+do
+  echo "Killing child process $child because ppid = $RUNSERVER_PID"
+  kill $child
+done
+
+rm $RUNSERVER_PID_FILE
+rm $CMSSERVER_PID_FILE
